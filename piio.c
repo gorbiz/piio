86ce103c0b8510b5fc9936e1a04b8d5f8f1bf238
pellsson <pellelindblad@gmail.com>
2016-01-05 12:40:57 +0000

    git too hard

--- piio.c
+++ piio.c
@@ -1,6 +1,13 @@
+#include "piio.h"
+
+#ifndef PIIO_KERNEL_MODULE
+//
+// User mode headers
+//
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 #include <unistd.h>
 
 #include <sys/types.h>
@@ -10,31 +17,47 @@
 
 #include <stdint.h>
 
-#ifdef PIIO_CLAM_READ
+static int piio_model		= 0;
+static int piio_board_rev	= 0;
+
+#else
+//
+// Check compile-time configuration
+//
+#ifndef PIIO_KERNEL_GPIO_BASE
+	#error To build PIIO for kernel mode, you must define PIIO_KERNEL_GPIO_BASE to either PIIO_KERNEL_GPIO_BASE_PI1 or PIIO_KERNEL_GPIO_BASE_PI2
+#endif
+
+#if PIIO_KERNEL_GPIO_BASE == PIIO_KERNEL_GPIO_BASE_PI2
+	#ifndef PIIO_KERNEL_BOARD_REV
+		#define PIIO_KERNEL_BOARD_REV PIIO_BOARD_REV_2
+	#elif PIIO_KERNEL_BOARD_REV != PIIO_BOARD_REV_2
+		#error Invalid configuration, PIIO_KERNEL_GPIO_BASE requires PIIO_KERNEL_BOARD_REV == PIIO_BOARD_REV_2
+	#endif
+#elif PIIO_KERNEL_GPIO_BASE == PIIO_KERNEL_GPIO_BASE_PI1
+	#ifndef PIIO_KERNEL_BOARD_REV
+		#error You must define PIIO_KERNEL_BOARD_REV to build PIIO as a kernel module
+	#elif (PIIO_KERNEL_BOARD_REV != PIIO_BOARD_REV_1) && (PIIO_KERNEL_BOARD_REV != PIIO_BOARD_REV_2)
+		#error Invalid value PIIO_KERNEL_BOARD_REV
+	#endif
+#else
+	#error Invalid value of PIIO_KERNEL_GPIO_BASE
+#endif
+//
+// Kernel mode headers
+//
+#include <asm/io.h>
+#include <linux/kernel.h>
+
+static const int piio_board_rev = PIIO_KERNEL_BOARD_REV;
+
+#endif
+
+#ifdef PIIO_CLAMP_READ
 	#define PIIO_FIX_LEVEL(x) !(!(x))
 #else
 	#define PIIO_FIX_LEVEL(x) (x)
 #endif
-
-typedef enum piio_error
-{
-	PIIO_ERROR_NONE = 0,
-	PIIO_NOT_ROOT,
-	PIIO_UNSUPPORTED_PI,
-	PIIO_FAILED_TO_MAP,
-	PIIO_INVALID_PIN,
-	PIIO_INVALID_MODE,
-}
-piio_error_t;
-
-#define PIIO_OK PIIO_ERROR_NONE
-
-typedef enum piio_pin_mode
-{
-	PIIO_INPUT,
-	PIIO_OUTPUT
-}
-piio_pin_mode_t;
 
 #pragma pack(push, 1)
 
@@ -52,20 +75,10 @@ piio_gpio_t;
 
 #pragma pack(pop)
 
-#ifndef TRUE
-#define TRUE 1
-#endif
-
-#ifndef FALSE
-#define FALSE 0
-#endif
-
-typedef int bool_t;
-
 #define PIIO_MAX_PINS 64
 
 static volatile piio_gpio_t *gpio	= NULL;
-static const int *phys_to_bcm		= NULL;
+static const int *pin_to_bcm		= NULL;
 static const int *gpio_to_bcm		= NULL;
 
 static const uint8_t bcm_to_rwreg[] =
@@ -123,7 +136,7 @@ static const int gpio_to_bcm_v2[PIIO_MAX_PINS] =
 } ;
 
 
-static const int phys_to_bcm_v1[PIIO_MAX_PINS] =
+static const int pin_to_bcm_v1[PIIO_MAX_PINS] =
 {
 	-1,
 	-1,	-1,
@@ -145,7 +158,7 @@ static const int phys_to_bcm_v1[PIIO_MAX_PINS] =
 	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
 };
 
-static const int phys_to_bcm_v2[PIIO_MAX_PINS] =
+static const int pin_to_bcm_v2[PIIO_MAX_PINS] =
 {
 	-1,
 	-1,	-1,
@@ -187,41 +200,83 @@ static const int phys_to_bcm_v2[PIIO_MAX_PINS] =
 	-1,	-1,
 };
 
-static int get_pi_version(void)
+#ifndef PIIO_KERNEL_MODULE
+
+static const char *get_cpuinfo_param(const char *name, char *line)
 {
+	char *p = line + strlen(line) - 1;
+
+	if(0 != strncasecmp(line, name, strlen(name)))
+	{
+		return NULL;
+	}
+
+	while('\n' == *p || '\r' == *p)
+	{
+		*(p--) = 0;
+	}
+
+	while(p > line && !isspace(*p))
+	{
+		--p;
+	}
+
+	if(p == line)
+	{
+		return NULL;
+	}
+
+	return (p + 1);
+}
+
+static piio_error_t load_pi_version(void)
+{
+
 	FILE *fp;
 	char line[256];
-	int version = 0;
 
 	if(NULL == (fp = fopen("/proc/cpuinfo", "rb")))
 	{
-		return 0;
+		return PIIO_ERROR_UNEXPECTED;
 	}
 
 	while(NULL != fgets(line, sizeof(line), fp))
 	{
-		char *p = line + strlen(line) - 1;
+		const char *param;
 
-		if(0 != strncasecmp(line, "Hardware", 8))
+		if(NULL != (param = get_cpuinfo_param("Hardware", line)))
 		{
-			continue;
+			if(0 == strcasecmp(param, "BCM2709"))
+			{
+				piio_model		= 2;
+				piio_board_rev	= 2;
+
+				goto cleanup;
+			}
+			else if(0 == strcasecmp(param, "BCM2708"))
+			{
+				piio_model = 1;
+			}
+			else
+			{
+				return PIIO_ERROR_UNSUPPORTED_PI;
+			}
+		}
+		else if(NULL != (param = get_cpuinfo_param("Revision", line)))
+		{
+			const size_t offset = (strlen(param) - 4);
+
+			if(0 == strcmp(param + offset, "0002")
+			|| 0 == strcmp(param + offset, "0003"))
+			{
+				piio_board_rev = 1;
+			}
+			else
+			{
+				piio_board_rev = 2;
+			}
 		}
 
-		while('\n' == *p || '\r' == *p)
-		{
-			*(p--) = 0;
-		}
-
-		while(p > line && ' ' != *p)
-		{
-			--p;
-		}
-
-		if(0 == strcasecmp(p + 1, "BCM2709"))
-		{
-			version = 2;
-			goto cleanup;
-		}
 	}
 
 	//
@@ -230,38 +285,40 @@ static int get_pi_version(void)
 
 cleanup:
 	fclose(fp);
-	return version;
+	return (0 != piio_model) && (0 != piio_board_rev)
+		? PIIO_OK
+		: PIIO_ERROR_UNSUPPORTED_PI;
 }
 
-static piio_error_t map_peripherals(int version)
+static piio_error_t map_peripherals(void)
 {
 	static const int open_flags = (O_RDWR | O_CLOEXEC | O_SYNC);
 
-	piio_error_t rc	= PIIO_FAILED_TO_MAP;
-	uint32_t base	= 0;
+	piio_error_t rc	= PIIO_ERROR_FAILED_TO_MAP;
+	uint64_t base	= 0;
 
 	int fd = open("/dev/gpiomem", open_flags);
 
 	if(fd < 0)
 	{
-		if(2 == version)
+		if(2 == piio_model)
 		{
-			base = 0x3F000000;
+			base = PIIO_KERNEL_GPIO_BASE_PI2;
 		}
-		else if(1 == version)
+		else if(1 == piio_model)
 		{
-			base = 0x20000000;
+			base = PIIO_KERNEL_GPIO_BASE_PI1;
 		}
 		else
 		{
-			return PIIO_UNSUPPORTED_PI;
+			return PIIO_ERROR_UNSUPPORTED_PI;
 		}
 
 		fd = open("/dev/mem", open_flags);
 
 		if(fd < 0)
 		{
-			return PIIO_FAILED_TO_MAP;
+			return PIIO_ERROR_FAILED_TO_MAP;
 		}
 	}
 
@@ -275,85 +332,105 @@ cleanup:
 	return rc;
 }
 
+#else
+//
+// PIIO_KERNEL_MODULE
+//
+static piio_error_t map_peripherals(void)
+{
+	if(NULL == (gpio = (piio_gpio_t *)ioremap_nocache(PIIO_KERNEL_GPIO_BASE + 0x200000, 0x1000)))
+	{
+		return PIIO_ERROR_FAILED_TO_MAP;
+	}
+
+	return PIIO_OK;
+}
+
+#endif
+
 piio_error_t piio_initialize(void)
 {
 	piio_error_t rc;
-	int version;
 
+#ifndef PIIO_KERNEL_MODULE
 	if(0 != geteuid())
 	{
-		return PIIO_NOT_ROOT;
+		return PIIO_ERROR_NOT_ROOT;
 	}
+#endif
 
-	version = get_pi_version();
-
-	if(0 == version)
-	{
-		return PIIO_UNSUPPORTED_PI;
-	}
-
-	if(PIIO_OK != (rc = map_peripherals(version)))
+	if(PIIO_OK != (rc = load_pi_version()))
 	{
 		return rc;
 	}
 
-	phys_to_bcm = (1 == version) ? phys_to_bcm_v1 : phys_to_bcm_v2;
-	gpio_to_bcm = (1 == version) ? gpio_to_bcm_v1 : gpio_to_bcm_v2;
+	if(PIIO_OK != (rc = map_peripherals()))
+	{
+		return rc;
+	}
+
+	pin_to_bcm	= (1 == piio_board_rev) ? pin_to_bcm_v1 : pin_to_bcm_v2;
+	gpio_to_bcm	= (1 == piio_board_rev) ? gpio_to_bcm_v1 : gpio_to_bcm_v2;
 
 	return PIIO_OK;
 }
 
-static piio_error_t set_pin_mode(int bcm, piio_pin_mode_t mode)
+static piio_error_t set_pin_mode(int bcm, piio_mode_t mode)
 {
+#ifdef PIIO_CLUMSY
 	if(-1 == bcm)
 	{
-		return PIIO_INVALID_PIN;
+		return PIIO_ERROR_INVALID_PIN;
 	}
 	else
+#endif
 	{
 		volatile uint32_t * const modereg = gpio->modereg + bcm_to_modereg[bcm];
 		const uint32_t modeshift = bcm_to_modeshift[bcm];
 
-		if(PIIO_INPUT == mode)
+		if(PIIO_MODE_INPUT == mode)
 		{
 			*modereg = *modereg & ~(7 << modeshift);
+			return PIIO_OK;
 		}
-		else if(PIIO_OUTPUT == mode)
+#ifdef PIIO_CLUMSY
+		else if(PIIO_MODE_OUTPUT != mode)
 		{
-			*modereg = (*modereg & ~(7 << modeshift)) | (1 << modeshift);
+			return PIIO_ERROR_INVALID_MODE;
 		}
-		else
-		{
-			return PIIO_INVALID_MODE;
-		}
+#endif
+
+		*modereg = (*modereg & ~(7 << modeshift)) | (1 << modeshift);
 	}
 
 	return PIIO_OK;
 }
 
-piio_error_t piio_set_mode_phys(uint32_t phys, piio_pin_mode_t mode)
+piio_error_t piio_set_mode_pin(uint32_t pin, piio_mode_t mode)
 {
-	return set_pin_mode(phys_to_bcm[phys & (PIIO_MAX_PINS - 1)], mode);
+	return set_pin_mode(pin_to_bcm[pin & (PIIO_MAX_PINS - 1)], mode);
 }
 
-piio_error_t piio_set_mode_gpio(uint32_t gpio, piio_pin_mode_t mode)
+piio_error_t piio_set_mode_gpio(uint32_t gpio, piio_mode_t mode)
 {
 	return set_pin_mode(gpio_to_bcm[gpio & (PIIO_MAX_PINS - 1)], mode);
 }
 
 static uint32_t read_bcm(int bcm)
 {
+#ifdef PIIO_CLUMSY
 	if(-1 == bcm)
 	{
 		return 0;
 	}
+#endif
 
 	return PIIO_FIX_LEVEL(gpio->lvlreg[bcm_to_rwreg[bcm]] & (1 << (bcm & 0x1F)));
 }
 
-uint32_t piio_read_phys(uint32_t phys)
+uint32_t piio_read_pin(uint32_t pin)
 {
-	return read_bcm(phys_to_bcm[phys & (PIIO_MAX_PINS - 1)]);
+	return read_bcm(pin_to_bcm[pin & (PIIO_MAX_PINS - 1)]);
 }
 
 uint32_t piio_read_gpio(uint32_t gpio)
@@ -363,10 +440,12 @@ uint32_t piio_read_gpio(uint32_t gpio)
 
 static void write_bcm(int bcm, uint32_t val)
 {
+#ifdef PIIO_CLUMSY
 	if(-1 == bcm)
 	{
 		return;
 	}
+#endif
 
 	if(val)
 	{
@@ -378,9 +457,9 @@ static void write_bcm(int bcm, uint32_t val)
 	}
 }
 
-void piio_write_phys(uint32_t phys, uint32_t val)
+void piio_write_pin(uint32_t pin, uint32_t val)
 {
-	write_bcm(phys_to_bcm[phys & (PIIO_MAX_PINS - 1)], val);
+	write_bcm(pin_to_bcm[pin & (PIIO_MAX_PINS - 1)], val);
 }
 
 void piio_write_gpio(uint32_t gpio, uint32_t val)
@@ -399,13 +478,16 @@ int hack(void)
 {
 	uint32_t i;
 
-	printf("Pin 0: %d\n", piio_set_mode_phys(0, PIIO_OUTPUT));
-	printf("Pin 1: %d\n", piio_set_mode_phys(1, PIIO_OUTPUT));
-	printf("Pin 3: %d\n", piio_set_mode_phys(3, PIIO_OUTPUT));
+	printf("Pin 0: %d\n", piio_set_mode_pin(0, PIIO_MODE_OUTPUT));
+	printf("Pin 1: %d\n", piio_set_mode_pin(1, PIIO_MODE_OUTPUT));
+	printf("Pin 3: %d\n", piio_set_mode_pin(3, PIIO_MODE_OUTPUT));
 
 	for(i = 0; ; ++i)
 	{
-		piio_write_phys(3, i & 1);
+		piio_write_pin(3, i & 1);
+		sleep(1);
+
+		printf("Hello\n");
 	}
 }
 
